bf75f4cdea32cfcc121da49fe5846781be08dd11
prashant shukla <ankurshukla051@gmail.com>
2026-03-04 00:01:03 +0530

    Add files via upload

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 
 
 async function authMiddleware(req, res, next) {
+    // const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
 
     if (!token) {
